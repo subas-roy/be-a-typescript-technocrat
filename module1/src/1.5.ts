c00118351a9d6be700e9ffc113313876d6248945
subas-roy <subasroy46@gmail.com>
2024-11-01 05:43:31 +0600

    Object , Optional and Literal Types

---
+++ module1/src/1.5.ts
@@ -0,0 +1,17 @@
+// Reference Type --> Object
+
+const user : {
+  readonly company: string;
+  firstName: string;
+  middleName?: string // optional type
+  lastName: string;
+  isMarried: boolean;
+} = {
+  company: "ABC",
+  firstName: "Subas",
+  lastName: "Roy",
+  isMarried: true
+}
+
+
+user.company = 'fnm'
