bc23bc47c95c916d147c128493391d55abf25435
subas-roy <subasroy46@gmail.com>
2024-11-04 05:10:49 +0600

    Class and object

---
+++ Module3/3.1.ts
@@ -0,0 +1,17 @@
+{
+  // oop - class
+
+  class Animal {
+    constructor(public name: string, public species: string, public sound: string) {}
+
+    makeSound() {
+      console.log(`The ${this.name} says ${this.sound}`)
+    }
+  }
+
+  const dog = new Animal("German Shepard", "dog", "ghew ghew")
+  const cat = new Animal("Persian", "cat", "meaw meaw")
+
+  dog.
+
+}
